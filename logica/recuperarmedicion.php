615a056d69724f2723f4e2ccbfa26ccd320c1938
Ruiyu Chen <rchen1@epsg.upv.es>
2023-10-11 01:13:58 +0200

    recuperarMedicion funciona Completo
    Puede recuperar todas las mediciones de la db

--- logica/recuperarmedicion.php
+++ logica/recuperarmedicion.php
@@ -2,7 +2,6 @@
 // Include your database connection
 require_once '../../connection.php';
 
-// Check if it's a GET request
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $response = array();
 
@@ -13,29 +12,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $result = mysqli_query($conn, $query);
 
     if ($result) {
-        // Fetch the rows and store them in an array
+        // Fetch the rows and store them in an array under the "data" key
+        $data = array();
         while ($row = mysqli_fetch_assoc($result)) {
-            $response[] = $row;
+            $data[] = $row;
         }
 
         // Close the database connection
         mysqli_close($conn);
 
-        // Modify the key from "0" to "data"
-        if (isset($response[0])) {
-            $newResponse["data"] = $response[0];
-            unset($response[0]); // Remove the old key if needed
-            $response = $newResponse;
-        }
-
-        // Set the response header to indicate JSON content
-       
-        $response["success"] ="1";
-        $response["message"] ="success";
+        $response["data"] = $data;
+        $response["success"] = "1";
+        $response["message"] = "success";
         header('Content-Type: application/json');
-
-        echo json_encode($response);      
-        
+        echo json_encode($response);
     } else {
         // If there's an error in the query, send an error response
         $response['error'] = 'Error fetching data from the database';
@@ -46,6 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     http_response_code(405);
     echo json_encode(array('error' => 'Invalid request method'));
 }
+
 
 ?>
 
