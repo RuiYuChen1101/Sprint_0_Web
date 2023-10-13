59b0e3ef2c3f99b32138769966b4946b84f6c42a
Ruiyu Chen <rchen1@epsg.upv.es>
2023-10-13 20:51:58 +0200

    Test de prueba automatica para insertaMedicion

--- logica/recuperarmedicion.php
+++ logica/recuperarmedicion.php
@@ -1,24 +1,21 @@
 <?php
-// Include your database connection
+
 require_once '../../connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $response = array();
 
-    // Query to select data from the 'medicion' table
     $query = "SELECT * FROM medicion";
 
-    // Execute the query
     $result = mysqli_query($conn, $query);
 
     if ($result) {
-        // Fetch the rows and store them in an array under the "data" key
+       
         $data = array();
         while ($row = mysqli_fetch_assoc($result)) {
             $data[] = $row;
         }
 
-        // Close the database connection
         mysqli_close($conn);
 
         $response["data"] = $data;
@@ -27,12 +24,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         header('Content-Type: application/json');
         echo json_encode($response);
     } else {
-        // If there's an error in the query, send an error response
+        
         $response['error'] = 'Error fetching data from the database';
         echo json_encode($response);
     }
 } else {
-    // If the request method is not GET, respond with a "405 Method Not Allowed" status code
+
     http_response_code(405);
     echo json_encode(array('error' => 'Invalid request method'));
 }
--- ux/logicaFake/recuperarmedicion.js
+++ ux/logicaFake/recuperarmedicion.js
@@ -1,22 +1,21 @@
 function recuperarmedicion(cb) {
-    // preparar la llamada remota
+   
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
-        // callback for when the response arrives
+        
         if (this.readyState == 4 && this.status == 200) {       
 
             var resultado = JSON.parse(this.responseText);
             if (resultado.success == "1") {
-                // Data was retrieved successfully
+                
                 cb(null, resultado);
             } else {
-                // Error occurred on the server side
+               
                 cb("Error: " + resultado.message, null);
             }
         }
     };
 
-    // call the remote file recuperarmedicion.php
     xmlhttp.open("GET", "../logica/recuperarmedicion.php", true);
     xmlhttp.send();
 }
